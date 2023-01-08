a2a7897101059dc7d67c4f673545570a0d1d3a5d
jhCOR <jhjung9759@naver.com>
2023-01-08 11:48:14 +0000

    update some new logic

--- main.kt
+++ main.kt
@@ -2,10 +2,25 @@ fun main(args: Array<String>) {
     printValue("Hello World")
 	printValue(10)
     
-    printValue(sum(1f,2f))
+    printValue(sum(1,2))
     printValue(multiply(1,2))
+    
     printValue(divide(1,2, {num:Number->if(num == 0){println("divide with zero error")}}))
-    printValue(divide(1,0, {num:Number->if(num == 0){println("divide with zero error")}}))
+    printValue(divide(1,0){num:Number->if(num == 0){println("divide with zero error")}})
+    
+    calculateAndPrint(1, 2, ::sum)
+    iterateExecute(5, {index:Int->println(index.toString())})
+}
+
+fun iterateExecute(a:Int, func:(Int)->Unit):Unit{
+    for (i in 1..a){
+        func(i)
+    }
+}
+
+fun calculateAndPrint(a:Number, b:Number, func:(Number, Number)->Float):Unit{
+    val result:String = func(a, b).toString()
+    println(result)
 }
 
 fun printValue(a:Any):Unit{
@@ -17,15 +32,15 @@ fun printValue(a:Any):Unit{
     }
 }
 
-fun sum(a:Float, b:Float):Float{
-    return a+b
+fun sum(a:Number, b:Number):Float{
+    return a.toFloat()+b.toFloat()
 }
 
-fun multiply(a:Number, b:Number):Number{
+fun multiply(a:Number, b:Number):Float{
     return a.toFloat()*b.toFloat()
 }
 
-inline fun divide(a:Number, b:Number, option:(num:Number)->Unit):Number{
+inline fun divide(a:Number, b:Number, option:(num:Number)->Unit):Float{
     option(b)
     if(b==0){
         var b_2:Number = b.toFloat()+0.00001
@@ -33,5 +48,4 @@ inline fun divide(a:Number, b:Number, option:(num:Number)->Unit):Number{
     }else{
         return a.toFloat()/b.toFloat()
     }
-    
 }
